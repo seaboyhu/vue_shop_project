0d90dd98efc3a509ef29d1ea5939b4ca2eafca2f
胡刘洋 <huliuyangyouxiang@163.com>
2020-05-28 20:25:56 +0800

    完成用户列表功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,13 +1,56 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Tooltip } from 'element-ui'
 import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 //导入弹框提示组件
 import { Message } from 'element-ui'
+//导入主体后台的控件
+import { Container, Header,Aside,Main } from 'element-ui'
+//导入内容菜单
+import { Menu, Submenu,MenuItem } from 'element-ui'
+//导入面包屑控件
+import { Breadcrumb,
+BreadcrumbItem, 
+Card, 
+Row, 
+Col, 
+Table, 
+TableColumn, 
+Switch, 
+tooltip, 
+pagination, 
+Dialog,
+MessageBox } from 'element-ui'//tooltip要小xie
 
 Vue.use(Button)
+
 Vue.use(Form)//从官网下载之后需要在上面import导入之后再这里引用全局
 Vue.use(FormItem)//用到哪些组件了必须要在这里按需导入
 Vue.use(Input)
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(tooltip)
+Vue.use(pagination)
+Vue.use(Dialog)
+
+
 //Message和其他不一样，需要进行全局挂载
 Vue.prototype.$message = Message
+//MessageBox和其他不一样，也需要在这里进行全局挂载
+Vue.prototype.$confirm = MessageBox.confirm
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from './../components/Login.vue'
 import Home from './../components/Home.vue'
+import Welcome from './../components/Welcome.vue'
+import Users from './../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-    {path: '/', redirect:'/login'}, //访问'/'的时候重定向到'/login'
-    {path: '/login',component: Login},
-    {path: '/home',component: Home}
+const routes = [
+  { path: '/', redirect: '/login' }, //访问'/'的时候重定向到'/login'
+  { path: '/login', component: Login },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome },
+    { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -16,15 +25,15 @@ const router = new VueRouter({
 })
 
 //挂载一个路由导航守卫(注意这个要在定义routers实例化之后)
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   //to 将要访问的路径
   //from 代表从那个路径跳转而来
   //next 是一个函数，表示放行
   //next() 放行  next('/login') 强制跳转
-  if(to.path ==='/login') return next()
+  if (to.path === '/login') return next()
   //获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
